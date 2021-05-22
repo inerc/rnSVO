6cc48837467e70877c795d8adbf28c49a3b62449
Inerc <inerc@mail.ru>
2021-05-22 15:52:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import type {Node} from 'react';
+import {SafeAreaView, StyleSheet, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {RootNavigator} from './navigators/RootNavigator';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
